b671a3d37e30fbb1b707696b7ecb7e8fff76c162
Nikolai Andryuschenko <nikand15@gmail.com>
2019-03-03 15:22:32 -0800

    Add max height routes carousal

--- app/components/Lists/SnapCarousal/Views/index.tsx
+++ app/components/Lists/SnapCarousal/Views/index.tsx
@@ -2,6 +2,7 @@ import React, { PureComponent, ReactNode } from 'react';
 import { View } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import { Scaled } from '../../../../themes';
+import styles from './styles';
 
 type Props = {
   data: Array<any>;
@@ -13,9 +14,12 @@ class SnapCarousalView extends PureComponent<Props> {
   render() {
     return (
       <View
-        style={{
-          height: this.props.height,
-        }}
+        style={[
+          styles.container,
+          {
+            height: this.props.height,
+          },
+        ]}
       >
         <Carousel
           data={this.props.data}
